c7858b6945d653ce678a0abfd182a52652f3fc54
rcgc <a01702388@itesm.mx>
2021-11-04 02:39:04 -0600

    last changes

--- dt2.py
+++ dt2.py
@@ -1,8 +1,6 @@
-import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
 
