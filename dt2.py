fcee2d93b4d3ed794a8c783532524be8518f2216
rcgc <a01702388@itesm.mx>
2021-11-18 15:24:53 -0600

    dt2.py fixed!

--- dt2.py
+++ dt2.py
@@ -5,11 +5,12 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
 
 df = pd.read_csv('customer_segmentation.csv')
-df = df.drop(["CustomerID", "Gender", "Age"], axis=1)
+y = df['label']
+df = df.drop(["CustomerID", "Gender", "Age", "label"], axis=1)
 df.drop(df.columns[df.columns.str.contains('Unnamed', case=False)], axis=1, inplace=True)
 
 X = df
-y = df['label']
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
 clf = DecisionTreeClassifier(max_leaf_nodes=5, random_state=0)
